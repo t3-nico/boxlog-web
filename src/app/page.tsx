ddf70a6e137d0fdac3787af8ae8f05a64fd19cb2
tomoya takayasu <tomoyatakayasu@tomoyanoMacBook-Air-2.local>
2025-05-15 09:03:49 +0900

    feat: create next.js app with tailwind + typescript
    # Conflicts:
    #	.gitignore
    #	README.md

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-4xl font-bold text-blue-500 p-8">
+      🎉 BoxLog 王国へようこそ！
+    </div>
+  );
+}
