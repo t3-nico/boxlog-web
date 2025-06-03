71ab1bf4df570e86b90cd3ddf54d031ca62b1fc8
t3-nico <tomoya.tanaka.work@gmail.com>
2025-06-04 08:10:06 +0900

    fix(calendar): update page heading

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,10 +1,8 @@
-
-
-export default function CalenderPage() {
+export default function CalendarPage() {
     return (
       <main className="flex min-h-screen items-center justify-center">
         <h1 className="text-3xl font-bold text-blue-600">
-          🕵️‍♀️ Calender ページ（カレンダー）
+            🕵️‍♀️ Calendar ページ（カレンダー）
         </h1>
       </main>
     );
