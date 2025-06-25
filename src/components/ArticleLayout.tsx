487c63896b55223549cd169ae01b008d641eddb7
t3-nico <tomoya.tanaka.work@gmail.com>
2025-06-25 10:26:19 +0900

    fix: mark docs layout components as client

--- src/components/ArticleLayout.tsx
+++ src/components/ArticleLayout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { TableOfContents } from '@/components/TableOfContents'
 
--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import clsx from 'clsx'
 
 export function Prose<T extends React.ElementType = 'div'>({
