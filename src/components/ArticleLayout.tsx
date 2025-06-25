28c079a1ddb3c6529d1634cd4b7877fcf1c9fab4
t3-nico <tomoya.tanaka.work@gmail.com>
2025-06-25 11:06:15 +0900

    Include blog posts in TOC layout

--- src/components/ArticleLayout.tsx
+++ src/components/ArticleLayout.tsx
@@ -1,13 +1,10 @@
-import { Prose } from '@/components/Prose'
 import { HeroPattern } from '@/components/HeroPattern'
 
 export function ArticleLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <HeroPattern />
-      <article className="mx-auto max-w-3xl py-16">
-        <Prose>{children}</Prose>
-      </article>
+      {children}
     </>
   )
 }
